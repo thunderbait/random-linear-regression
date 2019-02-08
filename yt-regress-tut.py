0da69f93309d40e69ef3bda0c07818a5e8adb730
Sotiris Hadjipanayi <sothadjipanayi@gmail.com>
2019-02-08 13:16:14 +0000

    Update yt-regress-tut.py

--- yt-regress-tut.py
+++ yt-regress-tut.py
@@ -2,7 +2,7 @@ import pandas as pd
 import quandl, math
 import numpy as np
 from sklearn import preprocessing, svm
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
@@ -15,7 +15,7 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
 forecast_col = 'Adj. Close'
-df.fillna(-99999, inplace=True)
+df.fillna(-.99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
 
@@ -26,15 +26,16 @@ df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'],1))
 y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+y = np.array(df['label']
 
-X = X[:-forecast_out+1]
-df.dropna(inplace=True)
-y = np.array(df['label'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-print(len(X), len(y))
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
 
+print(accuracy)
 
 
 
